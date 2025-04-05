ad7e79f525a5637038be660d05ff086c110dbd2e
Edoardo Lolletti <edoardo762@gmail.com>
2025-04-05 05:33:06 +0200

    Sync
    https://github.com/edo9300/edopro/commit/fe13f17b116fafefe4c278cec888f318d1da1026

--- deps/gframe/event_handler.cpp
+++ deps/gframe/event_handler.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include "utils.h"
 #include "game_config.h"
 #include "client_field.h"
@@ -42,19 +43,6 @@
 
 namespace {
 
-inline void TriggerEvent(irr::gui::IGUIElement* target, irr::gui::EGUI_EVENT_TYPE type) {
-	irr::SEvent event;
-	event.EventType = irr::EET_GUI_EVENT;
-	event.GUIEvent.EventType = type;
-	event.GUIEvent.Caller = target;
-	ygo::mainGame->device->postEventFromUser(event);
-}
-
-inline void SetCheckbox(irr::gui::IGUICheckBox* chk, bool state) {
-	chk->setChecked(state);
-	TriggerEvent(chk, irr::gui::EGET_CHECKBOX_CHANGED);
-}
-
 #if EDOPRO_ANDROID || EDOPRO_IOS
 inline bool TransformEvent(const irr::SEvent& event, bool& stopPropagation) {
 	return porting::transformEvent(event, stopPropagation);
@@ -84,16 +72,7 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 			case BUTTON_HAND2:
 			case BUTTON_HAND3: {
 				mainGame->wHand->setVisible(false);
-				if(mainGame->dInfo.curMsg == MSG_ROCK_PAPER_SCISSORS) {
-					DuelClient::SetResponseI(id - BUTTON_HAND1 + 1);
-					DuelClient::SendResponse();
-				} else {
-					mainGame->stHintMsg->setText(L"");
-					mainGame->stHintMsg->setVisible(true);
-					CTOS_HandResult cshr;
-					cshr.res = id - BUTTON_HAND1 + 1;
-					DuelClient::SendPacketToServer(CTOS_HAND_RESULT, cshr);
-				}
+				SendRPSResult(id - BUTTON_HAND1 + 1);
 				break;
 			}
 			case BUTTON_FIRST:
@@ -2216,8 +2195,8 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event, bool& stopPropagation)
 		case irr::KEY_F9: {
 			if (!event.KeyInput.PressedDown) {
 				const auto new_val = !mainGame->current_topdown;
-				SetCheckbox(mainGame->gSettings.chkTopdown, new_val);
-				SetCheckbox(mainGame->gSettings.chkKeepFieldRatio, new_val);
+				GUIUtils::SetCheckbox(mainGame->device, mainGame->gSettings.chkTopdown, new_val);
+				GUIUtils::SetCheckbox(mainGame->device, mainGame->gSettings.chkKeepFieldRatio, new_val);
 			}
 			return true;
 		}
@@ -3071,6 +3050,19 @@ void ClientField::ShowPileDisplayCards(int location, int player) {
 	if(display_cards.size()) {
 		mainGame->wCardDisplay->setText(epro::format(L"{}({})", gDataManager->GetSysString(loc_id), display_cards.size()).data());
 		ShowLocationCard();
+	}
+}
+void ClientField::SendRPSResult(uint8_t i) {
+	assert((1 <= i) && (i <= 3));
+	if(mainGame->dInfo.curMsg == MSG_ROCK_PAPER_SCISSORS) {
+		DuelClient::SetResponseI(i);
+		DuelClient::SendResponse();
+	} else {
+		mainGame->stHintMsg->setText(L"");
+		mainGame->stHintMsg->setVisible(true);
+		CTOS_HandResult cshr;
+		cshr.res = i;
+		DuelClient::SendPacketToServer(CTOS_HAND_RESULT, cshr);
 	}
 }
 }
